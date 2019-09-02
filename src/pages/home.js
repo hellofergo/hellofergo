8c8cdd85c4a4b19cdaf6231c7a21c3cbc5d49676
Ashlee Boyer <43934258+ashleemboyer@users.noreply.github.com>
2019-09-02 16:58:43 -0400

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,23 +4,31 @@ import { Link } from "react-router-dom";
 import { getFirebase } from "../firebase";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
   const [blogPosts, setBlogPosts] = useState([]);
 
-  getFirebase()
-    .database()
-    .ref("/posts")
-    .orderByChild("date")
-    .once("value")
-    .then(snapshot => {
-      let posts = [];
-      const snapshotVal = snapshot.val();
-      for (let slug in snapshotVal) {
-        posts.push(snapshotVal[slug]);
-      }
+  if (loading && !blogPosts.length) {
+    getFirebase()
+      .database()
+      .ref("/posts")
+      .orderByChild("date")
+      .once("value")
+      .then(snapshot => {
+        let posts = [];
+        const snapshotVal = snapshot.val();
+        for (let slug in snapshotVal) {
+          posts.push(snapshotVal[slug]);
+        }
 
-      const newestFirst = posts.reverse();
-      setBlogPosts(newestFirst);
-    });
+        const newestFirst = posts.reverse();
+        setBlogPosts(newestFirst);
+        setLoading(false);
+      });
+  }
+  
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
@@ -34,7 +42,7 @@ const Home = () => {
           <div className="card-content">
             <h2>
               {blogPost.title} &mdash;{" "}
-              <span style={{ color: "#5e5e5e" }}>{blogPost.date}</span>
+              <span style={{ color: "#5e5e5e" }}>{blogPost.datePretty}</span>
             </h2>
             <p
               dangerouslySetInnerHTML={{
