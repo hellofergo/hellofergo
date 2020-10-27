45d54e270e18ef0ef1425e871bfb083d6970017a
James Ferguson <jamesferguson@me.com>
2020-10-27 21:58:56 +1100

    protect create route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
+import ProtectedRoute from "./ProtectedRoute"
 import Home from "./pages/home";
 import Create from "./pages/create";
 import Post from "./pages/post";
@@ -18,7 +18,7 @@ function App() {
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/create" component={Create} />
+          <ProtectedRoute path="/create" component={Create} />
           <Route path="/404" component={NoMatch} />
           <Route path="/:slug" component={Post} />
         </Switch>
